8e2b4f030a8561552ee483b9fe344b74dd6a8c27
Aleksandr Harianin <onlydipro@gmail.com>
2023-06-29 21:49:56 +0300

    complete task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const body = document.querySelector('body');
+const buttonEle = document.querySelector('.change-color');
+let span = document.querySelector('.color');
+
+buttonEle.addEventListener('click', () => {
+  const currentColor = getRandomHexColor();
+ 
+
+  body.style.backgroundColor = currentColor;
+
+  span.textContent = `${currentColor}`;
+})
